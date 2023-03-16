1edc21643b2dea8d814bfba427db9467b0f227ba
kennyhip <nikitasobl1@gmail.com>
2023-03-16 18:42:12 +0600

    Homework

---
+++ Task 1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+Console.Clear();
+Console.Write("Введите пятизначное число: ");
+string number = Console.ReadLine()!;
+
+while (number.Length > 5 || number.Length < 5)
+{
+    Console.WriteLine("Вы ввели неверное число");
+    Console.Write("Введите пятизначное число: ");
+    number = Console.ReadLine()!;
+}
+
+int count = 0;
+
+for (int i = 0; i < (number.Length / 2); i++)
+{
+    if (number[i] == number[(number.Length-1)-i])
+        count = count + 1;
+}
+
+if (count == (number.Length / 2))
+    Console.WriteLine("Это число является палиндромом");
+else
+    Console.WriteLine("Это число не является палиндромом");
+
---
+++ Task 3/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.Clear();
+Console.Write("Введите натуральное число: ");
+int number = int.Parse(Console.ReadLine());
+
+while (number<1)
+{
+    Console.WriteLine("Вы ввели неверное число");
+    Console.Write("Введите натуральное число: ");
+    number = int.Parse(Console.ReadLine());
+}
+
+for (int i = 1; i <= number; i++)
+    Console.Write($"{Math.Pow(i, 3)}, ");
